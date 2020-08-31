d93ec9a4667a23028434a6c65a096432df4a1308
kensaku.komatsu <kensaku.komatsu@gmail.com>
2020-08-31 17:18:40 +0900

    for issue #19 - get usermedia

--- public/labs/web-codecs/script.js
+++ public/labs/web-codecs/script.js
@@ -1,11 +1,24 @@
+const sourceVideo = document.querySelector(".source video")
+
 const checkSupported = () => {
   return !!window.VideoEncoder
+}
+
+const getLocalMedia = async () => {
+  const stream = await navigator.mediaDevices.getUserMedia({video: true, audio: true})
+    .catch( err => { throw(err) })
+
+  return stream
 }
 
 //////////////////////////////////////////////////////////////////////////////
 const run = async () => {
   document.querySelector("#web-codecs-supported").innerHTML = 
     checkSupported() ? "yes" : "no"
+
+  const localStream = await getLocalMedia()
+  sourceVideo.srcObject = localStream
+  console.log( localStream)
 }
 
 run()
